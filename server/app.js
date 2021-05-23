d34fac86f1053d6a4e6083ea4c7a2b5a324deae7
Ikem Krueger <ikem.krueger@gmail.com>
2021-05-24 00:54:02 +0200

    Implemented 2/3 git commands

--- server/app.js
+++ server/app.js
@@ -1,5 +1,5 @@
 function log(fn, req, res) {
-	console.log(`${fn} => request: ${req.method} '${req.url}' => response:\n${JSON.stringify(res, null, 2)}`);
+    console.log(`${fn} => request: ${req.method} '${req.url}'\n`);
 }
 
 const execFile = require('child_process').execFile;
@@ -12,51 +12,38 @@ app.use(cors());
 app.use(express.static('client'));
 
 app.get('/', (req, res) => {
-	res.send(`http://localhost:${port}/index.html`);
+    res.send(`http://localhost:${port}/index.html`);
 });
 
 app.get('/history', (req, res) => {
-	execFile('git', ['-C', path, 'log', '--oneline'], (error, stdout, stderr) => {
-		let messages = [];
+    execFile('git', ['-C', path, 'log', '--oneline'], (error, stdout, stderr) => {
+        let messages = stdout.replace(/([a-z0-9]{7}) (.*)/g, '{ "hash": "$1", "message": "$2" }, ').replace(/\n/g, ' ').replace(/^/, "[ ").replace(/},  $/, "} ]");
+    
+        log("populateCommitHistory()", req, messages);
 
-		const regex = /([a-z0-9]{7}) (.*)/g;
+        res.type('json');
 
-		let lines = stdout.split("\n");
-
-		lines.pop(); // FIXME: last element is empty
-
-		lines.forEach((line) => {
-			let result = line.replace(regex, '{ "hash": "$1", "message": "$2" }');
-			
-			messages.push(JSON.parse(result));
-		});
-
-		log("populateCommitHistory()", req, messages);
-
-		res.json(messages);
-	});
+        res.send(messages);
+    });
 });
 
 app.get('/tree', (req, res) => {
-	// git -C <path> ls-tree -r <commit>
-	let files = [
-		{ "hash": "8799ad7eade90b481d06fb1703fd6c464210e367", "file": "Links.txt" }, 
-		{ "hash": "97219aebe1d4be4c7df577b9f15922610468fa92", "file": "header-link-emperor.css" }, 
-		{ "hash": "76815ee992673236b838629a77acbcdb428562c1", "file": "header-link-emperor.js" }, 
-		{ "hash": "90687de26d71e91b7c82565772a7df470ae277a6", "file": "icons/header-link-emperor-48.png" }, 
-		{ "hash": "a1129b78e86930bda45c7ca18806ffd1bca606ca", "file": "manifest.json" }
-	]
-	
-	log("populateCommitHistory()", req, files);
-
-    res.json(files);
+    execFile('git', ['-C', path, 'ls-tree', '-r', '468a330'], (error, stdout, stderr) => {
+        let files = stdout.replace(/([0-9]{6}) (blob) ([a-z0-9]{40})\t(.*)/g, '{ "hash": "$3", "file": "$4"}, ').replace(/\n/g, ' ').replace(/^/, "[ ").replace(/},  $/, "} ]");
+    
+        log("populateFilesystemTree()", req, files);
+        
+        res.type('json');
+        
+        res.send(files);
+    });
 });
 
 app.get('/file', (req, res) => {
     // git -C <path> show <commit>
-	let content = ".headerlink {\n    display:none;\n    margin:0 0 0 .2em;\n    text-decoration:none;\n    color:#999;\n}\n\nh1:hover *,\nh2:hover *,\nh3:hover *,\nh4:hover *,\nh5:hover *,\nh6:hover * {\n    display:inline;\n}\n";
-	
-	log("populateCommitHistory()", req, content); // FIXME
+    let content = ".headerlink {\n    display:none;\n    margin:0 0 0 .2em;\n    text-decoration:none;\n    color:#999;\n}\n\nh1:hover *,\nh2:hover *,\nh3:hover *,\nh4:hover *,\nh5:hover *,\nh6:hover * {\n    display:inline;\n}\n";
+    
+    log("populateFileContent()", req, content);
 
     res.send(content);
 });
--- server/client/index.js
+++ server/client/index.js
@@ -2,112 +2,112 @@ const httpRequest = async (url, format) => {
     try {
         const response = await fetch(url);
 
-		if(format == "json") {
-			return await response.json();
-		} else {
-			return await response.text();
-		}
+        if(format == "json") {
+            return await response.json();
+        } else {
+            return await response.text();
+        }
     } catch(e) {
         console.error(e);
     }
 }
 
 function dropCommitHistory() {
-	let history = document.querySelector("#history");
-	
-	history.innerHTML = "";
+    let history = document.querySelector("#history");
+    
+    history.innerHTML = "";
 }
 
 async function populateCommitHistory() {
-	let history = document.querySelector("#history");
+    let history = document.querySelector("#history");
 
-	let path = document.querySelector("#path").value;
+    let path = document.querySelector("#path").value;
 
-	let orderByLastCommit = true;
-	
-	let messages = await httpRequest(host + '/history', format="json");
+    let orderByLastCommit = true;
+    
+    let messages = await httpRequest(host + '/history', format="json");
 
-	messages.forEach(item => {
-		let option = document.createElement("option");
-		
-		option.value = item.hash;
-		option.innerText = item.message;
-		
-		orderByLastCommit ? history.append(option) : history.prepend(option);
-	});
-	
-	history.selectedIndex = 0; // FIXME: hardcoded value
+    messages.forEach(item => {
+        let option = document.createElement("option");
+        
+        option.value = item.hash;
+        option.innerText = item.message;
+        
+        orderByLastCommit ? history.append(option) : history.prepend(option);
+    });
+    
+    history.selectedIndex = 0; // FIXME: hardcoded value
 
-	populateFilesystemTree();
+    populateFilesystemTree();
 }
 
 function dropFilesystemTree() {
-	let tree = document.querySelector("#tree");
+    let tree = document.querySelector("#tree");
 
-	tree.innerHTML = "";
+    tree.innerHTML = "";
 }
 
 async function populateFilesystemTree() {
-	let history = document.querySelector("#history");
-	let tree = document.querySelector("#tree");
+    let history = document.querySelector("#history");
+    let tree = document.querySelector("#tree");
 
-	let path = document.querySelector("#path").value;
-	let commit = history.value;
-	
-	let files = await httpRequest(host + '/tree', format="json");
+    let path = document.querySelector("#path").value;
+    let commit = history.value;
+    
+    let files = await httpRequest(host + '/tree', format="json");
 
-	files.forEach(item => {
-		let option = document.createElement("option");
-		
-		option.value = item.hash;
-		option.innerText = item.file;
-		
-		tree.append(option);
-	});
+    files.forEach(item => {
+        let option = document.createElement("option");
+        
+        option.value = item.hash;
+        option.innerText = item.file;
+        
+        tree.append(option);
+    });
 
-	tree.selectedIndex = 0; // FIXME: hardcoded value
+    tree.selectedIndex = 0; // FIXME: hardcoded value
 
-	populateFileContent();
+    populateFileContent();
 }
 
 function dropFileContent(){
-	let file = document.querySelector("#file");
-	
-	file.innerHTML = "";
+    let file = document.querySelector("#file");
+    
+    file.innerHTML = "";
 }
 
 async function populateFileContent() {
-	let tree = document.querySelector("#tree");
-	let file = document.querySelector("#file");
-	
-	let path = document.querySelector("#path").value;
-	let commit = tree.value;
+    let tree = document.querySelector("#tree");
+    let file = document.querySelector("#file");
+    
+    let path = document.querySelector("#path").value;
+    let commit = tree.value;
 
-	let content = await httpRequest(host + '/file', format="text");
+    let content = await httpRequest(host + '/file', format="text");
 
-	file.value = content;
+    file.value = content;
 }
 
 function main() {
-	let history = document.querySelector("#history");
+    let history = document.querySelector("#history");
 
-	history.addEventListener("change", (event) => { slider.value = (history.selectedIndex + 1); });
-	history.addEventListener("change", (event) => { dropFilesystemTree(); populateFilesystemTree(); });
+    history.addEventListener("change", (event) => { slider.value = (history.selectedIndex + 1); });
+    history.addEventListener("change", (event) => { dropFilesystemTree(); populateFilesystemTree(); });
 
-	populateCommitHistory();
+    populateCommitHistory();
 
-	let slider = document.querySelector("#slider");
-	
-	slider.min = 1;
-	slider.value = history.selectedIndex + 1;
-	slider.max = history.length; // FIXME
+    let slider = document.querySelector("#slider");
+    
+    slider.min = 1;
+    slider.value = history.selectedIndex + 1;
+    slider.max = history.length; // FIXME
 
-	slider.addEventListener("change", (event) => { history.selectedIndex = (slider.value - 1); });
-	slider.addEventListener("change", (event) => { dropFilesystemTree(); populateFilesystemTree(); });
-	
-	let tree = document.querySelector("#tree");
-	
-	tree.addEventListener("change", (event) => { dropFileContent(); populateFileContent(); });
+    slider.addEventListener("change", (event) => { history.selectedIndex = (slider.value - 1); });
+    slider.addEventListener("change", (event) => { dropFilesystemTree(); populateFilesystemTree(); });
+    
+    let tree = document.querySelector("#tree");
+    
+    tree.addEventListener("change", (event) => { dropFileContent(); populateFileContent(); });
 }
 
 var port = "3000";
